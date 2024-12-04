f8bdc9269f9adf8c6bf21fbf9a1b70e06a581791
Ian Weiss <iweiss.anim@gmail.com>
2024-12-04 15:19:25 -0600

    Knight Turn defined as a function

--- rpg.py
+++ rpg.py
@@ -1,7 +1,31 @@
 def main():
     battling = True
-    knight = 200
-    wizard = 150
-    dentist = 100
-    if (knight < 0) and (wizard < 0) and (knight < 0):
+    knight_hp = 200
+    wizard_hp = 150
+    dentist_hp = 100
+    boss_hp = 1000
+    if (knight_hp <= 0) and (wizard_hp <= 0) and (dentist_hp <= 0):
         battling = False
+    if (boss_hp <= 0)
+        battling = False
+
+def knight_turn(knight_hp, boss_hp):
+    if knight_hp > 0:
+        knight_move_choice = str(input("Knight: ATTACK or DEFEND"))
+        if knight_move_choice == "ATTACK":
+            boss_hp == boss_hp - 40
+            print("Knight struck valiantly for 40 damage!")
+        elif knight_move_choice == "DEFEND":
+            if knight_cooldown = False:
+                is_knight_defending = True
+                knight_cooldown = True
+            elif knight_cooldown = True:
+                print("Knight cannot defend twice in a row!")
+                
+
+
+            
+
+
+def gameloop():
+    knight_turn():
