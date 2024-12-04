2c401a177406d74f3b246cdab569e25c4cb83ff6
Ian Weiss <iweiss.anim@gmail.com>
2024-12-04 17:02:03 -0600

    Knight attack implemented, but the boss cannot die.

--- rpg.py
+++ rpg.py
@@ -1,31 +1,35 @@
-def main():
-    battling = True
-    knight_hp = 200
-    wizard_hp = 150
-    dentist_hp = 100
-    boss_hp = 1000
-    if (knight_hp <= 0) and (wizard_hp <= 0) and (dentist_hp <= 0):
-        battling = False
-    if (boss_hp <= 0)
-        battling = False
+battling = True
+knight_hp = 200
+wizard_hp = 150
+dentist_hp = 100
+boss_hp = 1000
+if (knight_hp <= 0) and (wizard_hp <= 0) and (dentist_hp <= 0):
+    battle_lost = True
+    battling = False
+if (boss_hp <= 0):
+    battle_won = True
+    battling = False
 
-def knight_turn(knight_hp, boss_hp):
+def knight_turn():
+    global boss_hp
     if knight_hp > 0:
-        knight_move_choice = str(input("Knight: ATTACK or DEFEND"))
+        knight_move_choice = str(input("Knight: ATTACK or DEFEND\n"))
         if knight_move_choice == "ATTACK":
-            boss_hp == boss_hp - 40
+            boss_hp = boss_hp - 500
             print("Knight struck valiantly for 40 damage!")
         elif knight_move_choice == "DEFEND":
-            if knight_cooldown = False:
+            if knight_cooldown == False:
                 is_knight_defending = True
                 knight_cooldown = True
-            elif knight_cooldown = True:
+                print("Knight raises their shield! Next damage negated!")
+            elif knight_cooldown == True:
                 print("Knight cannot defend twice in a row!")
-                
 
+while battling is True:
+    knight_turn()
 
-            
-
-
-def gameloop():
-    knight_turn():
+while battling is False:
+    if battle_lost == True:
+        print("The party was defeated...")
+    if battle_won == True:
+        print("The party was victorious!")
